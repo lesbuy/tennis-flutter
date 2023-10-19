c475d9cf6f1d2b3de908bb3bb607fdde24be1c34
lesbuy <lesbuy@gmail.com>
2023-10-19 16:49:24 +0800

    home grid

--- build_web.sh
+++ build_web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+flutter build web --web-renderer html
 tar czf web.tar.gz build/web/*
-scp web.tar.gz 66.42.39.26:/tmp
+scp web.tar.gz ubuntu@66.42.39.26:/tmp
---
+++ lib/components/iconfont.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IconFont {
+  static const IconData rank =
+      IconData(0xe66f, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData trophy =
+      IconData(0xe619, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData single =
+      IconData(0xe615, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData double =
+      IconData(0xe617, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData male =
+      IconData(0xe616, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData female =
+      IconData(0xe618, fontFamily: 'iconfont', matchTextDirection: true);
+  static const IconData refresh =
+      IconData(0xe656, fontFamily: 'iconfont', matchTextDirection: true);
+}
